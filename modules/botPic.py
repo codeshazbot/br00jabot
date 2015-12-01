79edfc3b0d2140306866aacb4b1c3652eebdc811
davtheman <publicdavman@gmail.com>
2015-12-01 10:23:11 -0430

    autismo avanzado

--- modules/botPic.py
+++ modules/botPic.py
@@ -5,10 +5,9 @@ import random
 # Muh shitty def
 def getRandomPic():
     temp = []
-    randomPic = random.randrange(0, 24)
     r = praw.Reddit('Random Picture Grab')
     for submission in r.get_subreddit('lolcats').get_new(limit=25):
         temp.append(submission.url)
-    return temp[randomPic]
+    return random.choice(temp)
     
 # bot loads a link and telegram takes care of loading the preview so it's fine
