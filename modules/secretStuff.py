fb818c972c89e6bb9c778df26ad38fa557b8e8b8
davtheman <publicdavman@gmail.com>
2015-12-19 21:20:52 -0430

    edited something, plz do not spy ;_;

---
+++ modules/secretStuff.py
@@ -0,0 +1,12 @@
+"""Get the token key from json file."""
+import json
+import urllib
+
+response = urllib.urlopen("https://api.myjson.com/bins/2ofe5")
+getJson = json.load(response)
+
+def getApiJson():
+    apiToken = []
+    for key in getJson:
+        apiToken.append(key['api'])
+    return apiToken
