83be08280424681c601c24b2877235ed60c08e59
Daniel M <ddisastrous@gmail.com>
2015-12-22 02:20:06 -0430

    added important stuff

--- main.py
+++ main.py
@@ -1,23 +1,25 @@
-import telegram
 from telegram import Updater
 updater = Updater(token='<API_TOKEN_HERE>')
 dispatcher = updater.dispatcher
 
+
 def start(bot, update):
-    bot.sendMessage(chat_id=update.message.chat_id, text="Soy un bot lacra, llegate")
-    
+    bot.sendMessage(chat_id=update.message.chat_id, text="Soy es lacra, llegate")
+
+
 def echo(bot, update):
     bot.sendMessage(chat_id=update.message.chat_id, text=update.message.text)
+
 
 def unknown(bot, update):
     bot.sendMessage(chat_id=update.message.chat_id, text="Soy marico y me meto comandos por el culo, lo siento, ese tambien me lo meti")
 
-def help(bot,update):
-    bot.sendMessage(chat_id=update.message.chat_id, text="Bot de pruebas, no esperes nada... Todavia.")
+
+def help(bot, update):
+    bot.sendMessage(chat_id=update.message.chat_id, text="lolnada")
 
 dispatcher.addUnknownTelegramCommandHandler(unknown)
 dispatcher.addTelegramMessageHandler(echo)
 dispatcher.addTelegramCommandHandler('start', start)
 dispatcher.addTelegramCommandHandler('help', help)
 updater.start_polling()
-
